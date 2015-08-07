2c49255bad98508a27cab28ca0f268f0dd43487a
lockinto <924354349@qq.com>
2015-08-07 11:25:43 +0800

    增加字符模式下的显卡驱动，修正文件夹位置为scipts，修改入口文件进行测试

--- init/entry.c
+++ init/entry.c
@@ -1,26 +1,9 @@
-#include "types.h"
+#include "console.h"
 
 int kern_entry()
 {
-	uint8_t *input = (uint8_t *)0xB8000;
-	uint8_t color = (0 << 4) | (15 & 0x0F);
-
-	*input++ = 'H'; *input++ = color;
-	*input++ = 'e'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'l'; *input++ = color;
-	*input++ = 'o'; *input++ = color;
-	*input++ = ','; *input++ = color;
-	*input++ = 'O'; *input++ = color;
-	*input++ = 'S'; *input++ = color;
-	*input++ = ' '; *input++ = color;
-	*input++ = 'K'; *input++ = color;
-	*input++ = 'E'; *input++ = color;
-	*input++ = 'R'; *input++ = color;
-	*input++ = 'N'; *input++ = color;
-	*input++ = 'E'; *input++ = color;
-	*input++ = 'L'; *input++ = color;
-	*input++ = '!'; *input++ = color;
+	console_clear();
+	console_write_color("hello,os kernel!\n",rc_black, rc_green);
 
 	return 0;
 }
