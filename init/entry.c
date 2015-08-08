2c786a20ec50ca12a72b1a59fb53043f4cdb488c
lockinto <924354349@qq.com>
2015-08-08 17:06:21 +0800

    更改entry

--- init/entry.c
+++ init/entry.c
@@ -1,15 +1,20 @@
-/* 
+/*
 * @Author: into9
 * @Date:   2015-08-06 22:35:17
 * @Last Modified by:   into9
-* @Last Modified time: 2015-08-08 11:09:54
+* @Last Modified time: 2015-08-08 17:05:44
 */
 #include "console.h"
 
 int kern_entry()
 {
-	console_clear();
-	console_write_color("Hello,os kernel!\nWelcome to my OS!\nHava a good day!\n",rc_black, rc_red);
+	init_debug();
 
-	return 0;
+    console_clear();
+
+    printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
+
+    panic("test");
+
+    return 0;
 }
