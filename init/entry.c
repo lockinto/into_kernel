718864299c7561cebffa23e76e99ed007ca64148
lockinto <924354349@qq.com>
2015-08-08 22:31:36 +0800

    0

--- init/entry.c
+++ init/entry.c
@@ -2,19 +2,17 @@
 * @Author: into9
 * @Date:   2015-08-06 22:35:17
 * @Last Modified by:   into9
-* @Last Modified time: 2015-08-08 17:05:44
+* @Last Modified time: 2015-08-08 22:30:42
 */
 #include "console.h"
 
 int kern_entry()
 {
 	init_debug();
+    init_gdt();
 
     console_clear();
-
     printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
-
-    panic("test");
 
     return 0;
 }
